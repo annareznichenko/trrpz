f8e185760473c4423fd103585c95ff852966388f
Anna <anna.reznichenko@nure.ua>
2021-05-16 21:13:17 +0300

    laba1 done

---
+++ WebApplication4/WebApplication4/Paint.js
@@ -0,0 +1,62 @@
+﻿function onClickButton2() {
+    var canvas = document.getElementById('canv');
+    var ctx = canvas.getContext('2d');
+    ctx.beginPath();
+    ctx.moveTo(1, 1);
+    ctx.lineTo(100, 100);
+    ctx.stroke();
+    ctx.closePath();
+
+    ctx.fillStyle = "blue";
+    ctx.font = 'bold 14px sans-serif';
+    ctx.fillStyle = 'blue';
+    ctx.textBaseline = "top";
+    ctx.fillText("TEXT", 20, 20);
+}
+
+function getXmlHttp() {
+    var xmlhttp;
+    try {
+        xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
+    } catch (e) {
+        try { xmlhttp = new ActiveXObject("Microsoft.XMLHTTP"); }
+        catch (E) { xmlhttp = false; }
+    }
+    if (!xmlhttp && typeof XMLHttpRequest != 'undefined') {
+        xmlhttp = new XMLHttpRequest();
+    }
+    return xmlhttp;
+}
+
+function onClickAJAX() {
+    var xmlhttp = getXmlHttp();
+    xmlhttp.open('GET', 'TextFile.txt', true);// false - синхронно
+    xmlhttp.send(null);
+    //-------------------------------
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 404) {
+            return;
+        }
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var Textout = xmlhttp.responseText;
+            PaintText(Textout);
+        }// ==4 ==200
+    }//onreadystatechange 
+    //---------------------------------
+}
+
+function PaintText(txt) {
+    var canvas = document.getElementById('canv1');
+    var ctx = canvas.getContext('2d');
+    ctx.beginPath();
+    ctx.moveTo(1, 1);
+    ctx.lineTo(100, 100);
+    ctx.stroke();
+    ctx.closePath();
+
+    ctx.fillStyle = "blue";
+    ctx.font = 'bold 14px sans-serif';
+    ctx.fillStyle = 'blue';
+    ctx.textBaseline = "top";
+    ctx.fillText(txt, 20, 20);
+}
---
+++ WebApplication4/WebApplication4/default.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="WebApplication4._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <link id ="style1" rel="stylesheet" type="text/css" href="style/StyleSheet.css"/>
+    <script src="Paint.js"></script>
+</head>
+    <body>
+        <form id="form1" runat="server">
+            <img alt="" class="auto-style1" src="clock.jpg" />
+            <p>
+                <asp:Label id="Time" runat="server" Text="Bремя: "/>
+            </p>
+            <p>
+                <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Refresh" />
+            </p>
+        </form>
+        <form>
+
+            <input id="Button2" type="button" value="Paint" onclick="onClickButton2()"/>
+            <div id="viewContainer" >
+                <canvas id="canv" style="padding: 0px; margin: 0px; position:relative; top: 0px; left: 0px; height: 90px; width: 632px;" ></canvas>
+            </div>
+        </form>
+        <form>
+
+            <input id="Button3" type="button" value="Paint" onclick="onClickAJAX()"/>
+            <div id="viewContainer1" >
+                <canvas id="canv1" style="padding: 0px; margin: 0px; position:relative; top: 0px; left: 0px; height: 90px; width: 632px;" ></canvas>
+            </div>
+        </form>
+    </body>
+</html>
